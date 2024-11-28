d2e5e33036ed38755fef3fbe8db4e3d4babf91c3
i4m-r4vi <ayswarym04@gmail.com>
2024-11-28 22:06:57 +0530

    Last Commit

--- frontend/src/components/ListPost.jsx
+++ frontend/src/components/ListPost.jsx
@@ -23,7 +23,7 @@ const ListPost = () => {
   useEffect(() => {
     const arrayData = async () => {
       try {
-        const listPost = await axios.get(`${process.env.BACKEND_URL}api/posts`);
+        const listPost = await axios.get(`${process.env.BACKEND_URL}/api/posts`);
         setPost(listPost.data);
       } catch (error) {
         setOpenError((prev)=>!prev)
