1ad421760821f58198626f6e673f7bbaf7ba5100
i4m-r4vi <ayswarym04@gmail.com>
2024-11-28 20:47:06 +0530

    Path Changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,4 @@
 import React from 'react'
-import AddPost from './components/addPost'
-import UpdatePost from './components/updatePost'
-import NotFound from './components/NotFound'
-import ListPost from './components/ListPost'
-import LoadingSpinner from './components/LoadingSpinner'
 
 
 const App = () => {
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,5 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Routes,Route,BrowserRouter} from 'react-router'
 import AddPost from './components/addPost.jsx'
